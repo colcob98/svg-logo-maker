20b8e7ef48272ac92444486e116d23a5cc23d9dc
Collier Cobb <collierj.cobb@gmail.com>
2023-12-29 12:15:20 -0500

    added questions and shapes

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const questions = []
+const questions = [
+  {
+    type: "list",
+    message: "Please select a logo shape.",
+    choices: ["Circle", "Square", "Triangle"],
+    name: "shape",
+  },
+  {
+    type: "input",
+    message: "Please select the color of the previously selected shape.",
+    name: "shapeColor",
+  },
+  {
+    type: "input",
+    name: "logoText",
+    message: "Please enter your text for the logo (No more than 3 characters).",
+    validate: (text) =>
+      text.length <= 3 || "Please enter text no longer than 3 characters.",
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Please enter your desired text color.",
+  },
+];
 
 function initPrompting() {
-    inquirer.prompt(questions).then((response)=> {
-        console.log(res);
-    });
+  inquirer.prompt(questions).then((response) => {
+    console.log(response);
+  });
 }
+
+initPrompting();
